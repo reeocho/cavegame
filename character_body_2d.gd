54794b802d868d9e54a74a5df9d0db7d91951151
reeocho <ziyao.chang@gmail.com>
2026-02-28 22:58:28 +1100

    finished movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+var up = preload("res://images/IMG_0060.PNG_scaled_8x_pngcrushed.png")
+var right = preload("res://images/IMG_0061.PNG_scaled_8x_pngcrushed.png")
+var down = preload("res://images/IMG_0059.PNG_scaled_8x_pngcrushed.png")
+var leftside = preload("res://images/leftside.png")
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -19,11 +23,19 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_just_pressed("ui_right"):
+			$Sprite2D.texture = right
+		if Input.is_action_just_pressed("ui_left"):
+			$Sprite2D.texture = leftside
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	var y_axis := Input.get_axis("ui_up", "ui_down")
 	if y_axis:
 		velocity.y = y_axis * SPEED
+		if Input.is_action_just_pressed("ui_up"):
+			$Sprite2D.texture = up
+		if Input.is_action_just_pressed("ui_down"):
+			$Sprite2D.texture = down
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
