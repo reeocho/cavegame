c42664720686d261036bd5ab0597b920150bebe6
reeocho <ziyao.chang@gmail.com>
2026-03-01 00:38:24 +1100

    loading screen fix

--- Scenes/area_2d.gd
+++ Scenes/area_2d.gd
@@ -2,19 +2,16 @@ extends Area2D
 
 var is_reachable: bool = false
 
-func _on_body_entered(body: Node2D) -> void:
-	# Check if the entering body is the player (you might need a group or specific naming for this check)
+func _on_body_entered(body: CharacterBody2D) -> void:
 	if body.name == "Player": 
 		is_reachable = true
 		print("Player entered interaction range.")
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(body: CharacterBody2D) -> void:
 	if body.name == "Player":
 		is_reachable = false
 		print("Player exited interaction range.")
 
 func _process(_delta: float) -> void:
-	if is_reachable and Input.is_action_just_pressed("interact"):
+	if is_reachable and Input.is_action_just_pressed("Interact"):
 		print("Interacted!")
-		# Add the specific action here (e.g., open a chest, start dialogue)
-		# Example: start_dialogue() 
--- loading_screen.gd
+++ loading_screen.gd
@@ -2,18 +2,17 @@ extends Node2D
 
 @onready var progress_bar = $ProgressBar
 @export var next_scene_path:String = "res://Scenes/main.tscn"
-
 var progress: Array[float] = []
 
 func _ready():
 	ResourceLoader.load_threaded_request(next_scene_path)
 	
-func process(delta):
+func _process(delta):
 	var status = ResourceLoader.load_threaded_get_status(next_scene_path, progress)
 	match status:
 		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 			var percentage = progress[0] * 100
 			progress_bar.value = percentage
 		ResourceLoader.THREAD_LOAD_LOADED:
-			var scene = ResourceLoader.load_threaded_get(next_scene_path)
-			get_tree().change_scene_to_packed(scene)
+			var scened = ResourceLoader.load_threaded_get(next_scene_path)
+			get_tree().change_scene_to_packed(scened)
